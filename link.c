ee2c3225cd4d47de18e49941822099e766506ab6
shibatj <phanuwat606@gmail.com>
2025-02-22 07:11:11 +0000

    .

--- link.c
+++ link.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-struct person{
+struct person
+{
     char name[50];
-    int no;
     int age;
+    int no;
 
     struct person *next_node;
 };
@@ -17,16 +20,79 @@ person_type *last_ptr;
 
 int menu()
 {
-    int choice = 0;
-    printf("Menu\n1.AddName\n2.Setting\n3.Search\n4.Exit\nEnter Choice >> ");
+    int choice;
+    printf("MENU\n1.Add_person\n2.Display_person\n3.Exit\nEnter Choice >> ");
     scanf("%d", &choice);
-
     return choice;
 }
 
-
-
-int main()
+void add_person()
 {
+    char name[50];
+    int age;
+    int no = count;
+    printf("Enter Name >> ");
+    scanf("%s", name);
+    printf("Enter Age >> ");
+    scanf("%d", &age);
+    count++;
 
+    person_type *node = malloc(sizeof(person_type));
+    strcpy(node->name,name);
+    node->age = age;
+    node->no = no;
+    node->next_node = NULL;
+
+    if(last_ptr == 0){
+        start_ptr = node;
+        last_ptr = node;
+    }else{
+        last_ptr->next_node=node;
+        last_ptr=node;
+    }
+}
+
+void display_person(person_type *p)
+{
+    printf("\nNumber >> %d\n", p->no);
+    printf("Name >> %s\n", p->name);
+    printf("Age >> %d\n", p->age);
+}
+
+void display_all()
+{
+    if(last_ptr == 0) return; //display nothing
+
+    person_type *current_node_ptr = start_ptr;
+    while(current_node_ptr != 0){
+        display_person(current_node_ptr);
+        current_node_ptr = current_node_ptr->next_node;
+    }
+}
+
+int main(){
+
+    start_ptr = NULL;
+    last_ptr = NULL;
+
+    int choice;
+
+    do{
+        choice = menu();
+
+        if(choice == 1){
+            add_person();
+        }
+        else if(choice == 2){
+            display_all();
+        }
+        else if(choice == 3){
+            printf("Exit!!");
+        }
+        else{
+            printf("Invalid key EIEI");
+        }
+    }while(choice != 3);
+
+    return 0;
 }
